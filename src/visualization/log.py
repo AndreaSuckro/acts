7b7651120a16e83e3308187c14f4ddfe9f4e38a3
andreasuckro <a.suckro@gmail.com>
2017-06-12 11:44:51 +0200

    adding linebreaks for log

--- src/visualization/log.py
+++ src/visualization/log.py
@@ -8,5 +8,5 @@ def log_results(epochs, losses, *, log_path='.'):
     """
     file = open(log_path + datetime.datetime.today().strftime('%Y-%m-%d-%H-%M')+'_perf.txt', 'w+')
     for ep, loss in zip(epochs, losses):
-        file.write(f'Epoch: {ep} has loss {loss}')
+        file.write(f'Epoch: {ep} has loss {loss} \n')
     file.close()
