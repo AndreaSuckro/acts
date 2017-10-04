8a9a677fb922f3ee1b319fac8a36f9a3e32a0cc8
andreasuckro <a.suckro@gmail.com>
2017-10-04 17:30:59 +0200

    first take on visualizing kernels

--- src/analysis/conv_vis.py
+++ src/analysis/conv_vis.py
@@ -1,76 +1,75 @@
 import tensorflow as tf
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 
-def visualize_kernel(kernel, grid_Y, grid_X, pad=1):
+def get_activations(sess, kernel, placeholder, data):
     """
-    Visualize conv. features as an image (mostly for the 1st layer).
-    Place kernel into a grid, with some paddings between adjacent filters.
-
-    Args:
-      kernel:            tensor of shape [Y, X, NumChannels, NumKernels]
-      (grid_Y, grid_X):  shape of the grid. Require: NumKernels == grid_Y * grid_X
-                           User is responsible of how to break into two multiples.
-      pad:               number of black pixels around each filter (between them)
-
-    Return:
-      Tensor of shape [(Y+2*pad)*grid_Y, (X+2*pad)*grid_X, NumChannels, 1].
+    Runs the data through the kernel and returns the output in a plot.
     """
+    activations = sess.run(kernel,feed_dict={placeholder.name+":0": data})
+    plot_nn_filter(activations)
 
-    x_min = tf.reduce_min(kernel)
-    x_max = tf.reduce_max(kernel)
 
-    kernel1 = (kernel-x_min)/(x_max-x_min)
+def plot_nn_filter(units, columns=8, figsize=(20,20)):
+    """
+    Plots the kernel of the conv layer hopefully in a beautiful way.
+    """
+    print(f'Activations come in shape {units.shape}')
+    filters = units.shape[4]
+    plt.figure(1, figsize=figsize)
+    n_columns = columns
+    n_rows = math.ceil(filters / n_columns) + 1
+    print(f'Number of filters is {filters}, displaying in {n_columns}x{n_rows}')
 
-    # pad between the kernels
-    padded_kernels = tf.pad(kernel1, tf.constant([[pad,pad], [pad,pad], [0,0], [0,0]]), mode='CONSTANT')
+    for i in range(filters):
+        plt.subplot(n_rows, n_columns, i+1)
+        plt.title('Filter ' + str(i+1))
+        #print(units[:,:,:,:,1])
+        plt.imshow(np.squeeze(units[:,:,:,:,i]), interpolation="nearest", cmap="gray")
+    plt.show()
 
-    # X and Y dimensions, w.r.t. padding
-    Y = kernel1.get_shape()[0] + 2 * pad
-    X = kernel1.get_shape()[1] + 2 * pad
 
-    channels = kernel1.get_shape()[2]
+def inspect_variables(sess, full=False):
+    """
+    Inspects the variables stored in the session and prints out
+    information for the trainable variables as well as the placeholders.
+    """
+    print('#####List of all TRAINABLE variables#####\n')
+    kernels = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    print(f'Found {len(kernels)} trainable variables in the graph:\n')
+    for var in kernels:
+        print(var)
+        if full:
+            print(var.eval())
 
-    # put NumKernels to the 1st dimension
-    x2 = tf.transpose(padded_kernels, (3, 0, 1, 2))
-    # organize grid on Y axis
-    x3 = tf.reshape(x2, tf.pack([grid_X, Y * grid_Y, X, channels]))
+    print('\n#####List of placeholders#####\n')
+    placeholders = [x for x in sess.graph.get_operations() if "Placeholder" in x.name]
+    print(f'Found {len(placeholders)} placeholders in the graph:\n')
+    for ph in placeholders:
+        print(ph.name)
+        print(ph.get_attr("shape"))
 
-    # switch X and Y axes
-    x4 = tf.transpose(x3, (0, 2, 1, 3))
-    # organize grid on X axis
-    x5 = tf.reshape(x4, tf.pack([1, X * grid_X, Y * grid_Y, channels]))
+    return kernels, placeholders
 
-    # back to normal order (not combining with the next step for clarity)
-    x6 = tf.transpose(x5, (2, 1, 3, 0))
 
-    # to tf.image_summary order [batch_size, height, width, channels],
-    #   where in this case batch_size == 1
-    x7 = tf.transpose(x6, (3, 0, 1, 2))
-
-    # scale to [0, 255] and convert to uint8
-    return tf.image.convert_image_dtype(x7, dtype=tf.uint8)
+def get_conv_kernels(sess):
+    """
+    A function to get all the convolutional kernels from a graph.
+    """
+    kernels = [k for k in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES) if "conv/kernel" in k.name]
+    print(f'Found {len(kernels)} layers with convolutional kernels.')
+    return kernels
 
 
 if __name__ == "__main__":
-    sess = tf.Session()
     # First let's load meta graph and restore weights
     saver = tf.train.import_meta_graph('../../data/networks/huang1/acts_2017-09-19T12-37_Huang_no_scaling_50x50.meta')
-    saver.restore(sess, '../../data/networks/huang1/acts_2017-09-19T12-37_Huang_no_scaling_50x50')
 
-    for i in range(10):
-        test = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)[i]
-        print(test)
-
-    time.sleep(1)
-    print('Try to get values')
-    with tf.variable_scope('conv1', reuse=None):
-        tf.get_variable_scope().reuse_variables()
-        print(tf.get_variable('conv/kernel:0'))
-
-    # Visualize conv1 kernels
-    #with tf.variable_scope('conv1'):
-    #    tf.get_variable_scope().reuse_variables()
-    #    kernels = tf.get_variable('conv/kernel')
-    #    grid = visualize_kernel(kernels)
-    #    tf.image.summary('conv1/kernels', grid, max_outputs=1)
+    with tf.Session() as sess:
+        saver.restore(sess, '../../data/networks/huang1/acts_2017-09-19T12-37_Huang_no_scaling_50x50')
+        _, placeholders = inspect_variables(sess)
+        kernels = get_conv_kernels(sess)
+        get_activations(sess, kernels[0], placeholders[0], np.ones((1,50,50,5)))
