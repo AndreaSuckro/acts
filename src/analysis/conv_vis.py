56f581ec5701f65918bb8627c9674088c463b90b
andreasuckro <a.suckro@gmail.com>
2017-11-01 09:19:53 +0100

    working on visualization

--- src/analysis/conv_vis.py
+++ src/analysis/conv_vis.py
@@ -19,24 +19,6 @@ def get_activations(sess, kernel, input_data, phase, data):
     return activations
 
 
-def plot_nn_filter(activations, columns=8, figsize=(20,20)):
-    """
-    Plots the kernel of the conv layer hopefully in a beautiful way.
-    """
-    print(f'Activations come in shape {units.shape}')
-    filters = activations.shape[4]
-    plt.figure(1, figsize=figsize)
-    n_columns = columns
-    n_rows = math.ceil(filters / n_columns) + 1
-    print(f'Number of filters is {filters}, displaying in {n_columns}x{n_rows}')
-
-    for i in range(filters):
-        plt.subplot(n_rows, n_columns, i+1)
-        plt.title('Filter ' + str(i+1))
-        plt.imshow(np.squeeze(units[:,:,:,1,i]), interpolation="nearest", cmap="gray")
-    plt.show()
-
-
 def inspect_variables(sess, full=False):
     """
     Inspects the variables stored in the session and prints out
@@ -71,6 +53,24 @@ def get_conv_kernels(sess):
     return kernels
 
 
+def plot_nn_filter(activations, columns=8, figsize=(20,20)):
+    """
+    Plots the kernel of the conv layer hopefully in a beautiful way.
+    """
+    print(f'Activations come in shape {activations.shape}')
+    filters = activations.shape[4]
+    plt.figure(1, figsize=figsize)
+    n_columns = columns
+    n_rows = math.ceil(filters / n_columns) + 1
+    print(f'Number of filters is {filters}, displaying in {n_columns}x{n_rows}')
+    fig = plt.figure()
+    for i in range(filters):
+        plt.subplot(n_rows, n_columns, i+1)
+        plt.title('Filter ' + str(i+1))
+        plt.imshow(np.squeeze(activations[:,:,:,1,i]), interpolation="nearest", cmap="gray")
+    fig.show()
+
+
 def plot_pca(activations):
     """
     plot filters with pca to see if there is a difference between them
@@ -88,14 +88,14 @@ def plot_pca(activations):
     pca.fit(np.array(activations))
     transformed = pca.transform(activations)
 
-    #transformed = pca.fit_transform(activations)
     print(f'Explained variance = {pca.explained_variance_ratio_}')
     fig = plt.figure()
+    fig.suptitle('PCA of kernel activation of the first layer')
     ax = fig.add_subplot(111)
 
     for i in range(filters):
         ax.scatter(transformed[i][0], transformed[i][1], label='Filter '+str(i))
-    plt.show()
+    fig.show()
 
 if __name__ == "__main__":
     # First let's load meta graph and restore weights
@@ -109,9 +109,9 @@ if __name__ == "__main__":
         input_ph = placeholders[0]
         phase_ph = [k for k in sess.graph.get_operations() if "phase" in k.name][0]
         #print(phase_ph)
-        #get_activations(sess, kernels[0], input_ph, phase_ph, np.ones((1,50,50,5)))
-        #get_activations(sess, kernels[0], input_ph, phase_ph, np.zeros((1,50,50,5)))
         activations = get_activations(sess, kernels[0], input_ph, phase_ph, np.random.rand(1,50,50,5))
 
         plot_pca(activations)
-        #plot_nn_filter(activations)
+        plot_nn_filter(activations)
+        # to ensure that everything only closes when it's done
+        plt.show()
